e2f6bec5c0cb6aedd2caf8fc41e0dc4e74bfb23a
Jesse A Johnson <jesseajohnson@pm.me>
2021-08-31 16:29:38 -0400

    removing bad import

--- src/api/async.ts
+++ src/api/async.ts
@@ -1,5 +1,3 @@
-import { Storage } from './scheme';
-
 export enum Status {
   pending = 'pending',
   resolved = 'resolved',
@@ -11,34 +9,28 @@ export enum Cause {
   reset = 'reset'
 }
 
-export type Meta<Signature> = {
+export type Meta<S> = {
   status: Status;
-  data: Signature;
+  data: S;
   error: any;
 };
 
-export type StorageMeta<Signature> = Meta<Signature> & {
-  _type?: Storage;
-  _key?: string;
-  _suffix?: string;
-};
-
-export interface Helper<Signature> {
-  setData: (value: Signature) => void;
+export interface Helper<S> {
+  setData: (value: S) => void;
   setError: (value: any) => void;
-  run: (promise: Promise<Signature>, preserve?: boolean) => void;
+  run: (promise: Promise<S>, preserve?: boolean) => void;
   reset: () => void;
 }
 
-export type Action<Signature> =
-  | { type: Status.resolved; data: Signature }
-  | { type: Status.rejected; error: any; data: Signature }
-  | { type: Status.pending; preserve: boolean; data: Signature }
-  | { type: Cause.reset; data: Signature };
+export type Action<S> =
+  | { type: Status.resolved; data: S }
+  | { type: Status.rejected; error: any; data: S }
+  | { type: Status.pending; preserve: boolean; data: S }
+  | { type: Cause.reset; data: S };
 
-export type Initializer<Signature> = {
+export type Initializer<S> = {
   status: Status.idle;
-  data: Signature,
+  data: S,
   error: any,
   _suffix: string,
   _key: string,
