88137109ecd9833422a98c62c4c484807962b573
Jesse A Johnson <jesseajohnson@pm.me>
2021-09-11 18:11:40 -0400

    custom server url injection

--- src/api/const.ts
+++ src/api/const.ts
@@ -1,2 +1,3 @@
-export const storylab_base = "https://gitlab.com/api/v4";
+export const version = 'api/v4';
+export const storylab_base = process.env.SL_GL_BASE_URL ? `${process.env.SL_GL_BASE_URL}/${version}` : `https://gitlab.com/${version}`;
 export const project_path = `${storylab_base}/projects/${process.env.SL_GL_PROJECT_ID}`;
